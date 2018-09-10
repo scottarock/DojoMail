9853c56425849f8e5ea5470c686e709b38bd801a
Scott Rock <scottrockbiz@gmail.com>
2018-09-09 22:40:34 -0700

    Dojo Mail assignment for Coding Dojo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dojo-mail';
+
+  title: string = 'Dojo Mail';
+  emails: Array<Email> = [
+    {
+      email: 'bill@gates.com',
+      importance: true,
+      subject: 'New Windows',
+      content: 'Windows XI will launch in year 2100'
+    },
+    {
+      email: 'ada@lovelace.com',
+      importance: true,
+      subject: 'Programming',
+      content: 'Enchantress of Numbers'
+    },
+    {
+      email: 'john@carmac.com',
+      importance: false,
+      subject: 'Updated Algo',
+      content: 'New algorithm for shadow volumes'
+    },
+    {
+      email: 'gabe@newel.com',
+      importance: false,
+      subject: 'HL3!',
+      content: 'Just kidding...'
+    },
+  ];
+
+
+}
+
+interface Email {
+  email: string;
+  importance: boolean;
+  subject: string;
+  content: string;
 }
